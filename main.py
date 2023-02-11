09de59fbd0408d006ba7587e4eeccde2ba70c7b8
johntronik <g.by.john@gmail.com>
2023-02-11 14:41:45 +0900

    add sys version

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import sys
 import pandas as pd
 
 def main():
+    print(f'{sys.version=}')
     print(f'{pd.__version__=}')
     df = pd.DataFrame(['Hello World'], columns=['message'])
     print(df)
